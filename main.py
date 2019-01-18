e0dd3a53f4b399025b8798f33051bd03b6597524
Tomas <xxxxx@xxxx.com>
2019-01-18 20:11:07 -0300

    seeems to work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 from win10toast import ToastNotifier
+import time
 
 toaster = ToastNotifier()
 
-toaster.show_toast("Test","This is the first test!")
+while True:
+    time.sleep(1800) #waits 30 minutes
+    toaster.show_toast("Breath","Take a break, wake up and walk!")
+    time.sleep(300) #waits 5 minutes
+    toaster.show_toast("Work","You can get back to work!")
