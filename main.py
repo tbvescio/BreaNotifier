eb7a5b9b98f2284ba5e848ad9e0606100bca9401
Tomas <xxxxx@xxxx.com>
2019-01-18 21:04:37 -0300

    .

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ toaster = ToastNotifier()
 
 while True:
     time.sleep(1800) #waits 30 minutes
-    toaster.show_toast("Breath","Take a break, wake up and walk!")
+    toaster.show_toast("Breath","Wake up and strech yourself.")
     time.sleep(300) #waits 5 minutes
     toaster.show_toast("Work","You can get back to work!")
